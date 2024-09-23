1cca122b5f2c51e6a9f36b8002296a00ae562379
safin33221 <saifn33221@gmail.com>
2024-09-23 22:36:42 +0600

    function complete to check donation Amount is positive number

--- js/donate.js
+++ js/donate.js
@@ -1,41 +1,67 @@
 document.getElementById('donate-btn').addEventListener('click', function (event) {
-    event.preventDefault()
+    event.preventDefault();
     const donateAmount = getInputFildById('donate-amount');
-    let availableBalance = getTextFildById('donate-balance')
-    let currentBalance = getTextFildById('current-balance');
-    document.getElementById('donate-balance').innerText = availableBalance + donateAmount;
-    document.getElementById('current-balance').innerText = currentBalance - donateAmount;
 
-    let historySection = document.getElementById('history-section');
+    if(isNaN(donateAmount) || donateAmount <= 0){
+        alert("invalid Donation Amount");
+    }
+    else{
+
+        let availableBalance = getTextFildById('donate-balance')
+        let currentBalance = getTextFildById('current-balance');
+        document.getElementById('donate-balance').innerText = availableBalance + donateAmount;
+        document.getElementById('current-balance').innerText = currentBalance - donateAmount;
     
-    historySection.innerHTML += `
-    <div class="border-2 p-6 rounded-lg w-11/12 mx-auto mb-4">
-                    <h1 class="text-xl font-bold text-secondary">${donateAmount} Taka is Donated for famine-2024 at Feni, Bangladesh</h1>
-                    <p class="text-success text-lg">Date : ${new Date()}</p>
-                </div>
-    `
+        let historySection = document.getElementById('history-section');
+        
+        historySection.innerHTML += `
+        <div class="border-2 p-6 rounded-lg w-11/12 mx-auto mb-4">
+                        <h1 class="text-xl font-bold text-secondary">${donateAmount} Taka is Donated for Flood at Noakhali, Bangladesh</h1>
+                        <p class="text-success text-lg">Date : ${new Date()}</p>
+                    </div>
+        `
+        document.getElementById('my_modal_1').showModal();
+    }
+
 
 })
+
+
+
 document.getElementById('donate-btn2').addEventListener('click', function (event) {
     event.preventDefault()
     const donateAmount = getInputFildById('donate-amount2');
-    let availableBalance = getTextFildById('donate-balance2')
-    let currentBalance = getTextFildById('current-balance');
-    document.getElementById('donate-balance2').innerText = availableBalance + donateAmount;
-    document.getElementById('current-balance').innerText = currentBalance - donateAmount;
+    if(isNaN(donateAmount) || donateAmount <= 0){
+        alert("invalid donation Amount");
+    }else{
+        
+        let availableBalance = getTextFildById('donate-balance2')
+        let currentBalance = getTextFildById('current-balance');
+        document.getElementById('donate-balance2').innerText = availableBalance + donateAmount;
+        document.getElementById('current-balance').innerText = currentBalance - donateAmount;
+    
+    
+        let historySection = document.getElementById('history-section');
+        historySection.innerHTML += `
+        <div class="border-2 p-6 rounded-lg w-11/12 mx-auto mb-4">
+                        <h1 class="text-xl font-bold text-secondary">${donateAmount} Taka is Donated for Flood Relief in Feni,Bangladesh</h1>
+                        <p class="text-success text-lg">Date : ${new Date()}</p>
+                    </div>
+        `
+        document.getElementById('my_modal_1').showModal();
 
-
-    let historySection = document.getElementById('history-section');
-    historySection.innerHTML += `
-    <div class="border-2 p-6 rounded-lg w-11/12 mx-auto mb-4">
-                    <h1 class="text-xl font-bold text-secondary">${donateAmount} Taka is Donated for Flood Relief in Feni,Bangladesh</h1>
-                    <p class="text-success text-lg">Date : ${new Date()}</p>
-                </div>
-    `
+    }
 })
+
+
+
 document.getElementById('donate-btn3').addEventListener('click', function (event) {
     event.preventDefault()
     const donateAmount = getInputFildById('donate-amount3');
+    if(isNaN(donateAmount) || donateAmount <= 0){
+        alert("invalid donation Amount");
+    }else{
+
     let availableBalance = getTextFildById('donate-balance3')
     let currentBalance = getTextFildById('current-balance');
     document.getElementById('donate-balance3').innerText = availableBalance + donateAmount;
@@ -48,5 +74,9 @@ document.getElementById('donate-btn3').addEventListener('click', function (event
                     <h1 class="text-xl font-bold text-secondary">${donateAmount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h1>
                     <p class="text-success text-lg">Date : ${new Date()}</p>
                 </div>
+
     `
+    document.getElementById('my_modal_1').showModal();
+
+    }
 })
