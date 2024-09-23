6c6b9f1355ff7b3ee9ab0970fc24bd98101d9bc6
safin33221 <saifn33221@gmail.com>
2024-09-24 00:24:25 +0600

    complete responsive for mobile device

--- js/donate.js
+++ js/donate.js
@@ -1,23 +1,23 @@
 document.getElementById('donate-btn').addEventListener('click', function (event) {
     event.preventDefault();
     const donateAmount = getInputFildById('donate-amount');
+    let availableBalance = getTextFildById('donate-balance')
+    let currentBalance = getTextFildById('current-balance');
 
-    if(isNaN(donateAmount) || donateAmount <= 0){
+    if (isNaN(donateAmount) || donateAmount <= 0) {
         alert("invalid Donation Amount");
     }
-    else{
+    else {
 
-        let availableBalance = getTextFildById('donate-balance')
-        let currentBalance = getTextFildById('current-balance');
         document.getElementById('donate-balance').innerText = availableBalance + donateAmount;
         document.getElementById('current-balance').innerText = currentBalance - donateAmount;
-    
+
         let historySection = document.getElementById('history-section');
-        
+
         historySection.innerHTML += `
         <div class="border-2 p-6 rounded-lg w-11/12 mx-auto mb-4">
-                        <h1 class="text-xl font-bold text-secondary">${donateAmount} Taka is Donated for Flood at Noakhali, Bangladesh</h1>
-                        <p class="text-success text-lg">Date : ${new Date()}</p>
+                        <h1 class="text-lg lg:text-xl font-bold text-secondary">${donateAmount} Taka is Donated for Flood at Noakhali, Bangladesh</h1>
+                        <p class="text-success text-sm lg:text-lg">Date : ${new Date()}</p>
                     </div>
         `
         document.getElementById('my_modal_1').showModal();
@@ -31,21 +31,21 @@ document.getElementById('donate-btn').addEventListener('click', function (event)
 document.getElementById('donate-btn2').addEventListener('click', function (event) {
     event.preventDefault()
     const donateAmount = getInputFildById('donate-amount2');
-    if(isNaN(donateAmount) || donateAmount <= 0){
+    if (isNaN(donateAmount) || donateAmount <= 0) {
         alert("invalid donation Amount");
-    }else{
-        
+    } else {
+
         let availableBalance = getTextFildById('donate-balance2')
         let currentBalance = getTextFildById('current-balance');
         document.getElementById('donate-balance2').innerText = availableBalance + donateAmount;
         document.getElementById('current-balance').innerText = currentBalance - donateAmount;
-    
-    
+
+
         let historySection = document.getElementById('history-section');
         historySection.innerHTML += `
         <div class="border-2 p-6 rounded-lg w-11/12 mx-auto mb-4">
-                        <h1 class="text-xl font-bold text-secondary">${donateAmount} Taka is Donated for Flood Relief in Feni,Bangladesh</h1>
-                        <p class="text-success text-lg">Date : ${new Date()}</p>
+                        <h1 class="text-lg lg:text-xl font-bold text-secondary">${donateAmount} Taka is Donated for Flood Relief in Feni,Bangladesh</h1>
+                        <p class="text-success  text-sm lg:text-lg">Date : ${new Date()}</p>
                     </div>
         `
         document.getElementById('my_modal_1').showModal();
@@ -58,25 +58,25 @@ document.getElementById('donate-btn2').addEventListener('click', function (event
 document.getElementById('donate-btn3').addEventListener('click', function (event) {
     event.preventDefault()
     const donateAmount = getInputFildById('donate-amount3');
-    if(isNaN(donateAmount) || donateAmount <= 0){
+    if (isNaN(donateAmount) || donateAmount <= 0) {
         alert("invalid donation Amount");
-    }else{
+    } else {
 
-    let availableBalance = getTextFildById('donate-balance3')
-    let currentBalance = getTextFildById('current-balance');
-    document.getElementById('donate-balance3').innerText = availableBalance + donateAmount;
-    document.getElementById('current-balance').innerText = currentBalance - donateAmount;
+        let availableBalance = getTextFildById('donate-balance3')
+        let currentBalance = getTextFildById('current-balance');
+        document.getElementById('donate-balance3').innerText = availableBalance + donateAmount;
+        document.getElementById('current-balance').innerText = currentBalance - donateAmount;
 
 
-    let historySection = document.getElementById('history-section');
-    historySection.innerHTML += `
+        let historySection = document.getElementById('history-section');
+        historySection.innerHTML += `
     <div class="border-2 p-6 rounded-lg w-11/12 mx-auto mb-4">
-                    <h1 class="text-xl font-bold text-secondary">${donateAmount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h1>
-                    <p class="text-success text-lg">Date : ${new Date()}</p>
+                    <h1 class="text-lg lg:text-xll font-bold text-secondary">${donateAmount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h1>
+                    <p class="text-success  text-sm lg:text-lg">Date : ${new Date()}</p>
                 </div>
 
     `
-    document.getElementById('my_modal_1').showModal();
+        document.getElementById('my_modal_1').showModal();
 
     }
 })
